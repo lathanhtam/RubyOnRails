2123b5ff478c3c481017f60f6c29fd413c2c6f42
La Thanh Tam <tamlt2704@gmail.com>
2014-12-09 16:28:22 +0700

    finish user edit, update, index and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
-	def new
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
 	end
 
 	def show
@@ -23,6 +28,46 @@ class UsersController < ApplicationController
       end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile update"
+      redirect_to @user
+      #handle a successful update
+    else
+      render 'edit'
+    end
+  end
+
+  # Confirms a logged-in user.
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
+  # Confirms the correct user.
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+  
+  # Confirms an admin user.
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
   # ---
   private
 
@@ -30,4 +75,6 @@ class UsersController < ApplicationController
   	params.require(:user).permit(:name, :email, :password,
   		:password_confirmation)
   end
+
+
 end
