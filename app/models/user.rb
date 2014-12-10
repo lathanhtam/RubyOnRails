f5bc2a937b679f8a18a6c877bccf11e5095a9d66
La Thanh Tam <tamlt2704@gmail.com>
2014-12-10 14:02:53 +0700

    Add account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,6 +43,22 @@ class User < ActiveRecord::Base
 		BCrypt::Password.new(remember_digest).is_password?(remember_token)
 	end
 
+	#Returns true if the given token matches the digest
+	def authenticated?(attribute, token)
+		digest = send("#{attribute}_digest")
+		return false if digest.nil?
+		BCrypt::Password.new(digest).is_password?(token)
+	end
+
+	def activate
+		update_attribute(:activated,   true)
+		update_attribute(:activated_at, Time.zone.now)
+	end
+
+	def send_activation_email
+		UserMailer.account_activation(self).deliver_now
+	end
+
 	private
 		#convert email to all lower-case
 		def downcase_email
