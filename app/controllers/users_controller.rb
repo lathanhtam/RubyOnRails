35aec54c57ffcc3ddd29df11f712ea6a40af6933
La Thanh Tam <tamlt2704@gmail.com>
2014-12-11 13:23:21 +0700

    remove email activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,13 +36,13 @@ class UsersController < ApplicationController
 		if @user.save
       	# Handle a successful save.
         # UserMailer.account_activation(@user).deliver_now
-        @user.send_activation_email
+        # @user.send_activation_email
         flash[:info] = "Please check your email to activate your account"
-        redirect_to root_url
+        # redirect_to root_url
         
-       #  log_in @user
-      	# flash[:success] = "Welcome to the Sample App!"
-      	# redirect_to @user
+        log_in @user
+      	flash[:success] = "Welcome to the Sample App!"
+      	redirect_to @user
       	#redirect_to user_url(@user)
       else
       	render 'new'
