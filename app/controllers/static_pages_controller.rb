478e46be547c9c62a2067f343c6e8a3c9289abc8
La Thanh Tam <tamlt2704@gmail.com>
2014-12-11 11:22:07 +0700

    Fixbug for master

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,10 @@
 class StaticPagesController < ApplicationController
 
   def home
-  	@microposts = current_user.microposts.build if logged_in?
-  	@feed_items = current_user.feed.paginate(page: params[:page]) 
+    if logged_in?
+    	@microposts = current_user.microposts.build 
+    	@feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
 
   def help
